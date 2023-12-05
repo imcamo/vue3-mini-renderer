13490dfcaa2c043498b0301fb88a7f5b22fd83a8
zengzhaogen <camo@dingtalk.com>
2023-12-06 04:15:35 +0800

    feat: render 支持双端对比算法

--- main.js
+++ main.js
@@ -17,12 +17,24 @@ const vnode1 = {
   children: [
     {
       type: 'p',
-      children: 'hello1',
+      children: '1',
+      key: 1,
     },
     {
       type: 'p',
-      children: 'hello2',
-    }
+      children: '2',
+      key: 2,
+    },
+    {
+      type: 'p',
+      children: '3',
+      key: 3,
+    },
+    // {
+    //   type: 'p',
+    //   children: '3',
+    //   key: 3
+    // }
   ]
 };
 
@@ -32,17 +44,30 @@ const vnode1 = {
 const vnode2 = {
   type: 'div',
   props: {
-    class: 'div2',
+    class: 'div1',
+    id: 'vnode1',
   },
   children: [
     {
       type: 'p',
-      children: 'hello3',
+      children: '1',
+      key: 1,
     },
     {
       type: 'p',
-      children: 'hello4',
-    }
+      children: '3',
+      key: 3,
+    },
+    // {
+    //   type: 'p',
+    //   children: '2',
+    //   key: 2,
+    // },
+    // {
+    //   type: 'p',
+    //   children: '3',
+    //   key: 3
+    // }
   ]
 };
 
@@ -74,12 +99,13 @@ function createRenderer(options) {
     createText,
     setText,
     createComment,
+    nextSibling,
   } = options;
 
   /**
    * 挂载 vnode
    */
-  function mountElement(vnode, container) {
+  function mountElement(vnode, container, anchor) {
     const el = vnode.el = createElement(vnode.type);
     if (typeof vnode.children === 'string') {
       setElementText(el, vnode.children);
@@ -96,7 +122,7 @@ function createRenderer(options) {
     }
 
     // 插入
-    insert(el, container);
+    insert(el, container, anchor);
   }
 
   /**
@@ -113,7 +139,7 @@ function createRenderer(options) {
    * 更新 vnode 
    * 负责元素挂载以及对比新旧 vnode
    */
-  function patch(n1, n2, container) {
+  function patch(n1, n2, container, anchor) {
     // 如果旧节点存在，但是新旧节点 type不同，没有对比的意义
     // 重置 n1，直接挂载 n2
     if (n1 && n1.type !== n2.type) {
@@ -129,7 +155,7 @@ function createRenderer(options) {
     if (typeof type === 'string') {
       // 如果没有旧节点，直接挂载新节点，不需要对比
       if (!n1) {
-        mountElement(n2, container);
+        mountElement(n2, container, anchor);
       } else {
         // 新旧节点都存在，则进入对比
         patchElement(n1, n2);
@@ -219,8 +245,9 @@ function createRenderer(options) {
         // 旧节点为一组子节点，
         // 此时需要 diff 算法
         // 先暴力实现，逐个卸载旧节点，再逐个挂载新节点
-        n1.children.forEach(child => unmount(child));
-        n2.children.forEach(child => patch(null, child, container));
+        // patchChildrenDiff1(n1, n2, container);
+        // patchChildrenDiff2(n1, n2, container);
+        patchChildrenDiff3(n1, n2, container);
       } else {
         // 此时旧节点要么为文本子节点，要么为没有子节点
         // 此时只需要清空旧节点即可，然后将新节点逐个挂载
@@ -237,6 +264,143 @@ function createRenderer(options) {
         setElementText(container, '');
         // 旧节点没有子节点不需要处理
         // ...
+      }
+    }
+  }
+
+  /**
+   * diff 算法 1
+   * 暴力对比，对旧子节点逐个进行卸载，再对新节点逐个进行挂载 
+   */
+  function patchChildrenDiff1(n1, n2, container) {
+    n1.children.forEach(child => unmount(child));
+    n2.children.forEach(child => patch(null, child, container));
+  }
+
+  /**
+   * diff 算法 2
+   * 对比新旧节点长度，取最短的那个节点进行 patch，然后再判断新旧节点的长度
+   * 如果新节点长度大于旧节点长度说明需要挂载新的多余街道
+   * 如果旧节点长度大于新节点说明需要卸载多余的旧节点
+   */
+  function patchChildrenDiff2(n1, n2, container) {
+    const oldLen = n1.children.length;
+    const newLen = n2.children.length;
+    // 共有的长度，逐个进行 patch
+    const commLen = Math.min(oldLen, newLen);
+    for (let i = 0; i < commLen; i++) {
+      patch(n1.children[i], n2.children[i], container);
+    }
+    if (oldLen > newLen) {
+      // 说明旧节点多，需要卸载
+      for (let i = commLen; i < oldLen; i++) {
+        unmount(n1.children[i]);
+      }
+    } else if (newLen > oldLen) {
+      // 说明新节点多，需要挂载
+      for (let i = commLen; i < newLen; i++) {
+        patch(n1.children[i], n2.children[i], container);
+      }
+    }
+  }
+
+  /**
+   * diff 算法 3
+   * 双端对比算法，在新旧子节点头尾新增两个指针，然后一次头头、尾尾、头尾、尾头依次对比
+   */
+  function patchChildrenDiff3(n1, n2, container, anchor) {
+    const oldChildren = n1.children;
+    const newChildren = n2.children;
+
+    let oldStartdx = 0;
+    let newStartIdx = 0;
+    let oldEndIdx = oldChildren.length - 1;
+    let newEndIdx = newChildren.length - 1;
+
+    let oldStartVnode = oldChildren[oldStartdx];
+    let newStartVnode = newChildren[newStartIdx];
+    let oldEndVnode = oldChildren[oldEndIdx];
+    let newEndVnode = newChildren[newEndIdx];
+
+    while (newStartIdx <= newEndIdx && oldStartdx <= oldEndIdx) {
+      // oldStartVnode 或者 oldEndVnode 这个不存在证明在非常规的对比中找到复用的了
+      if (!oldStartVnode) {
+        oldStartVnode = oldChildren[++oldStartdx];
+      } else if (!oldEndVnode) {
+        oldEndVnode = oldChildren[--oldStartdx];
+      } else if (oldStartVnode.key === newStartVnode.key) {
+        // 头头对比
+        patch(oldStartVnode, newStartVnode, container);
+        oldStartVnode = oldChildren[++oldStartdx];
+        newStartVnode = newChildren[++newStartIdx];
+      } else if (oldEndVnode.key === newEndVnode.key) {
+        // 尾尾对比
+        patch(oldEndVnode, newEndVnode, container);
+        oldEndVnode = oldChildren[--oldEndIdx];
+        newEndVnode = newChildren[--newEndIdx];
+      } else if (oldStartVnode.key === newEndVnode.key) {
+        // 头尾对比
+        // 此时证明旧节点移动到尾部去了,需要将 oldStartVnode, 插入到 oldEndVnode 的后面
+        patch(oldStartVnode, newEndVnode, container);
+        insert(oldStartVnode.el, container, nextSibling(oldEndVnode.el));
+        oldStartVnode = oldChildren[++oldStartdx];
+        newEndVnode = newChildren[--newEndIdx];
+      } else if (oldEndVnode.key === newStartVnode.key) {
+        // 尾头对比
+        // 此时证明旧节点被移动到前面去了，需要将 oldEndVnode 移动到 oldStartVnode 前面
+        patch(oldEndVnode, newStartVnode, container);
+        insert(oldEndVnode.el, container, oldStartVnode.el);
+        oldEndVnode = oldChildren[--oldEndIdx];
+        newStartVnode = newChildren[++newStartIdx];
+      } else {
+        // 如果上述无法匹配
+        // 则查找非头部和非尾部是否有可复用的DOM
+        // 遍历旧节点，查找其中是否有新节点开始节点的相同的KEY
+        // 如果大于 0 则说明这个节点之前不是头部节点，但是现在尾头部节点，则需要把旧节点移动到oldStartVnode之前
+        // @example
+        // 新节点：1 2 3 4 （1这个节点在旧节点中的位置）
+        // 旧节点：2 4 1 3
+        const idxInOld = oldChildren.findIndex(
+          node => node.key === newStartVnode.key
+        );
+        if (idxInOld > 0) {
+          // 当前需要移动的旧节点
+          const moveNode = oldChildren[idxInOld];
+          patch(moveNode, newStartVnode, container);
+          // 插入到旧节点的最前面位置
+          insert(moveNode.el, container, oldStartVnode.el);
+          // 旧节点当前的元素已经被移走了，此时需要重置下
+          oldChildren[idxInOld] = undefined;
+          // 更新新节点的开始位置
+          newStartVnode = newChildren[++newStartIdx];
+        } else {
+          // 如果没有找到，说明新节点此时是新增的节点，只需要将他插入到旧节点最前面即可
+          // @example
+          // 新节点 4 3 1 2 （4 是新增的节点）
+          // 旧节点 1 2 3
+          patch(null, newStartVnode, container, oldStartVnode.el);
+          newStartVnode = newChildren[++newStartIdx];
+        }
+      }
+    }
+
+  
+    if (oldEndIdx < oldStartdx && newStartIdx <= newEndIdx) {
+      // 此时说明旧节点已经被遍历完了，但是新节点还有遍历完，需要新增新节点
+      // @example
+      // 新节点：4 1 2 3
+      // 旧节点：1 2 3
+      for (let i = newStartIdx; i <= newEndIdx; i++) {
+        const anchor = newChildren[newEndIdx + 1] ? newChildren[newEndIdx + 1].el : null;
+        patch(null, newChildren[i], container, anchor);
+      }
+    } else if (newEndIdx < newStartIdx && oldStartdx <= oldEndIdx) {
+      // 此时说明新节点被遍历完了，但是旧节点还没有被遍历完，需要删除旧节点
+      // @example
+      // 新节点：1 3
+      // 旧节点 1 2 3
+      for (let i = oldStartdx; i <= oldEndIdx; i++) {
+        unmount(oldChildren[i]);
       }
     }
   }
@@ -322,6 +486,10 @@ const renderer = createRenderer({
   // 创建注释节点
   createComment() {
     return document.createComment('');
+  },
+  // 当前元素的相邻元素
+  nextSibling(el) {
+    return el.nextSibling;
   }
 });
 
@@ -331,12 +499,12 @@ renderer.render(vnode1, document.querySelector('#app'));
 // 第二次渲染
 setTimeout(() => {
   console.log('元素更新');
-  renderer.render(vnode3, document.querySelector('#app'));
+  renderer.render(vnode2, document.querySelector('#app'));
 }, 3000);
 
 
 // 第三次次渲染
-setTimeout(() => {
-  console.log('元素卸载');
-  renderer.render(null, document.querySelector('#app'));
-}, 6000);
+// setTimeout(() => {
+//   console.log('元素卸载');
+//   renderer.render(null, document.querySelector('#app'));
+// }, 6000);
